d186ea1f45c4fc37d8f80f22180dbd5f3394b1fe
Fabian Schultz <uxhlv@student.kit.edu>
2024-10-11 09:33:42 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#define GAME_STATE_TESTING
+
+int main() {
+
+#ifndef GAME_STATE_TESTING
+    std::cout << "Hello, World!" << std::endl;
+#endif
+
+    // Doing all test for the gameState Class
+#ifdef GAME_STATE_TESTING
+
+
+
+
+#endif
+
+    return 0;
+}
